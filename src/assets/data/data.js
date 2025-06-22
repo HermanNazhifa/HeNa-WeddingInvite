19526f109ab13d479467d5b67423f86846fac21f
Gilang Aditya <langzaditya21@gmail.com>
2025-06-22 21:41:00 +0700

    Add Wedding Invitation File

---
+++ src/assets/data/data.js
@@ -0,0 +1,127 @@
+export const data = {
+  bride: {
+    L: {
+      id: 1,
+      name: "Herman (Kemod)",
+      child: "Putra bungsu",
+      father: "Alm. Bapak Andi",
+      mother: "Ibu Nani",
+      image: "./src/assets/images/cowo.png",
+    },
+    P: {
+      id: 2,
+      name: "Nazhifa Fauziah (Difa)",
+      child: "Putri ke 8",
+      father: "Bapak Mian",
+      mother: "Ibu Neneng",
+      image: "./src/assets/images/cewe.png",
+    },
+
+    couple: "./src/assets/images/couple.png",
+  },
+
+  time: {
+    marriage: {
+      year: "2025",
+      month: "Juli",
+      date: "03",
+      day: "Kamis",
+      hours: {
+        start: "10.00",
+        finish: "11.00",
+      },
+    },
+    reception: {
+      year: "2025",
+      month: "Juli",
+      date: "03",
+      day: "Kamis",
+      hours: {
+        start: "11.00",
+        finish: "Selesai",
+      },
+    },
+    address:
+      "Aula Assafarul Jannah Kp. Sawah Ilir, RT 002/RW 003, Desa Mekar Jaya, Kec. Ciomas, Kab. Bogor, Jawa Barat",
+  },
+
+  link: {
+    calendar: "https://calendar.app.google/m6V8vdFhVS6fbpbE8",
+    map: "https://maps.app.goo.gl/cWuQxirj8niMnqjM6",
+  },
+
+  galeri: [
+    {
+      id: 1,
+      image: "./src/assets/images/1.png",
+    },
+    {
+      id: 2,
+      image: "./src/assets/images/2.png",
+    },
+    {
+      id: 3,
+      image: "./src/assets/images/3.png",
+    },
+    {
+      id: 4,
+      image: "./src/assets/images/4.png",
+    },
+    {
+      id: 5,
+      image: "./src/assets/images/5.png",
+    },
+  ],
+
+  bank: [
+    {
+      id: 1,
+      name: "Lorem Ipsum",
+      icon: "./src/assets/images/bca.png",
+      rekening: "12345678",
+    },
+    {
+      id: 2,
+      name: "Ipsum Lorem",
+      icon: "./src/assets/images/bri.png",
+      rekening: "12345678",
+    },
+  ],
+
+  audio: "./src/assets/audio/wedding.mp3",
+
+  api: "https://script.google.com/macros/s/AKfycbz7j_-ydKTc39OMf2X5AqK0awtGKT-ZNGXLHUtiwlZO8RbjVxbVhmfifb9vL_bFiKob/exec",
+
+  navbar: [
+    {
+      id: 1,
+      teks: "Home",
+      icon: "bx bxs-home-heart",
+      path: "#home",
+    },
+    {
+      id: 2,
+      teks: "Mempelai",
+      icon: "bx bxs-group",
+      path: "#bride",
+    },
+    {
+      id: 3,
+      teks: "Tanggal",
+      icon: "bx bxs-calendar-check",
+      path: "#time",
+    },
+    {
+      id: 4,
+      teks: "Galeri",
+      icon: "bx bxs-photo-album",
+      path: "#galeri",
+    },
+    {
+      id: 5,
+      teks: "Ucapan",
+      icon: "bx bxs-message-rounded-dots",
+      path: "#wishas",
+    },
+  ],
+};
