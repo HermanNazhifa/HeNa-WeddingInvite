cbbf028de24c390a767d330cb37256425a1f6fbd
Gilang Aditya <langzaditya21@gmail.com>
2025-06-23 20:08:37 +0700

    Fix

--- src/js/galeri.js
+++ src/js/galeri.js
@@ -31,23 +31,11 @@ export const galeri = () => {
     const selectedImage = data.galeri.find((item) => item.id === id);
 
     if (selectedImage) {
-      const currentImg = figureElement.querySelector("img");
+      figureElement.innerHTML = `<img src="${selectedImage.image}" alt="galeri image" id="${selectedImage.id}">`;
 
-      if (currentImg) {
-        currentImg.classList.add("fade-out");
-
-        setTimeout(() => {
-          currentImg.src = selectedImage.image;
-          currentImg.id = selectedImage.id;
-          currentImg.alt = "galeri image";
-          currentImg.classList.remove("fade-out");
-
-          // Update pagination
-          paginationElement.querySelectorAll("li").forEach((li) => {
-            li.classList.toggle("active", parseInt(li.dataset.id) === id);
-          });
-        }, 300);
-      }
+      paginationElement.querySelectorAll("li").forEach((li) => {
+        li.classList.toggle("active", parseInt(li.dataset.id) === id);
+      });
     }
   };
 
